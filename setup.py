b7155a3d40b8f20b1e8b5ae468318641fcb1eafa
Khary A. Mendez <kharyam@gmail.com>
2022-07-21 19:59:50 -0400

    Prepare for release

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ import setuptools
 
 setuptools.setup(
     use_scm_version=False,
-    setup_requires=['setuptools_scm']
-)
+    setup_requires=['setuptools_scm'],
+    long_descrption='Litra Driver command line interface and graphical interface'
